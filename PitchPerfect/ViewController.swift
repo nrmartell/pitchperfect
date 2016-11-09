c684f3aa536e5cfe9d0228d3a5a80b3c348fa263
Natalie Martell <nrmartell@gmail.com>
2016-11-09 12:24:28 -0700

    views and button functionality

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
- @IBOutlet weak var recordingLabel: UILabel!
+
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,10 +31,19 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(_ sender: Any) {
         print("Record button pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
    
     @IBAction func stopRecording(_ sender: Any) {
-        print("Stop recording button pressed")
+        print("Stop button pressed")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
+    override func viewWillAppear(_ animated: Bool) {
+       
+    }
+   
     }
 
